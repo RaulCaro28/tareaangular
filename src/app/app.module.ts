281c79cc2cccac313fd61666aecfc42d48493062
RaulCaro28 <raulcaro329@gmail.com>
2022-04-08 15:08:29 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './home/header/header.component';
+import { ContentComponent } from './home/content/content.component';
+import { FooterComponent } from './home/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
